b035efd76090768995e3511241638ef5369edaf4
Jdavidaa01 <jdavidaa.01@gmail.com>
2024-09-03 17:32:10 -0500

    step 65

--- P5_CalorieCounter/script.js
+++ P5_CalorieCounter/script.js
@@ -11,12 +11,34 @@ function cleanInputString(str){
     const regex=/[+-\s]/g;
     return str.replace(regex,"");
 }
-//console.log(cleanInputString("+-99"));
+//console.log(cleanInputString("+-99"));const entryNumber = targetInputContainer.querySelectorAll();
 function isInvalidInput(str){
     const regex= /\d+e\d+/i;
     return str.match(regex);
 }
 function addEntry(){
-    const targetId = "#" + entryDropdown.value;
-    const targetInputContainer = document.querySelector(`${targetId} .input-container`);
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length+1;
+  const HTMLString=`<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">
+  
+  `;
+  targetInputContainer.insertAdjacentHTML("beforeend",HTMLString) ;
+}   
+//end build  dynamic HTML
+addEntryButton.addEventListener('click',addEntry);
+function getCaloriesFromInputs(list){
+  let calories=0;
+  for(const item of list){
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+    if (invalidInputMatch){
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError=true;
+      return null;
+    }
+  }
+  
 }
